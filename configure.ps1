ba579b0a563f41288f0362d64ec9c1dfe6049834
Matthew Millett <mattrm456@gmail.com>
2020-11-16 20:09:11 -0500

    Update

---
+++ configure.ps1
@@ -0,0 +1,131 @@
+# The Abraxas Project
+# Copyright (c) 2002-2020 Matthew Millett (mattrm456@gmail.com)
+
+Set-Variable -name CONFIGURE_REPOSITORY -value $PSScriptRoot
+Set-Variable -name CONFIGURE_UNAME -value "Windows"
+Set-Variable -name CONFIGURE_PREFIX -value "C:\gde"
+Set-Variable -name CONFIGURE_BUILD_DIRECTORY -value "$CONFIGURE_REPOSITORY\tmp"
+Set-Variable -name CONFIGURE_JOBS -value 8
+Set-Variable -name CONFIGURE_DEBUG -value $false
+Set-Variable -name CONFIGURE_OPTIMIZE -value $false
+Set-Variable -name CONFIGURE_CLEAN -value $false
+Set-Variable -name CONFIGURE_TEST -value $false
+Set-Variable -name CONFIGURE_WARN -value $false
+Set-Variable -name CONFIGURE_VERBOSE -value $false
+
+function Help {
+    $HELP_TEXT = @"
+usage: configure [--verbose] [--prefix <directory>]
+where:
+    --prefix     <directory>    Path to the installation directory [$CONFIGURE_PREFIX]
+    --build-dir  <directory>    Path to the generated cmake artifacts [$CONFIGURE_BUILD_DIRECTORY)
+    --jobs       <number>       Maximum number of parallel build jobs [$CONFIGURE_JOBS]
+    --clean                     Clean build directory before configuring
+    --debug                     Compile with debug symbols
+    --optimize                  Compile with optimizations
+    --test                      Enable test drivers
+    --warn                      Enable warnings
+    --verbose                   Generate makefiles with verbose output
+"@
+    Write-Output $HELP_TEXT
+}
+
+for ($i = 0; $i -lt $args.count; $i++) {
+    if ($args[$i] -eq "--help") { 
+        Help
+        exit 0
+    }
+    elseif ($args[$i] -eq "--prefix") { 
+        Set-Variable -name CONFIGURE_PREFIX -value $args[$i + 1]
+        $i++
+    }
+    elseif ($args[$i] -eq "--build-dir") { 
+        Set-Variable -name CONFIGURE_BUILD_DIRECTORY -value $args[$i + 1]
+        $i++
+    }
+    elseif ($args[$i] -eq "--clean") { 
+        Set-Variable -name CONFIGURE_CLEAN -value $true
+    }
+    elseif ($args[$i] -eq "--debug") { 
+        Set-Variable -name CONFIGURE_DEBUG -value $true
+    }
+    elseif ($args[$i] -eq "--optimize") { 
+        Set-Variable -name CONFIGURE_OPTIMIZE -value $true
+    }
+    elseif ($args[$i] -eq "--test") { 
+        Set-Variable -name CONFIGURE_TEST -value $true
+    }
+    elseif ($args[$i] -eq "--warn") { 
+        Set-Variable -name CONFIGURE_WARN -value $true
+    }
+    elseif ($args[$i] -eq "--verbose") { 
+        Set-Variable -name CONFIGURE_VERBOSE -value $true
+    }
+    else {
+        Help
+        exit 1
+    }
+}
+
+Set-Variable -Name CONFIGURE_BUILD_DIRECTORY -Value "$CONFIGURE_BUILD_DIRECTORY\obj"
+
+if ($CONFIGURE_CLEAN) {
+    if (Test-Path $CONFIGURE_BUILD_DIRECTORY) {
+        Remove-Item -Force -Recurse $CONFIGURE_BUILD_DIRECTORY
+        if (-not $?) {
+            Write-Output "Failed to clean build directory '$CONFIGURE_BUILD_DIRECTORY'"
+            exit 1
+        }
+    }
+}
+
+if (-not (Test-Path $CONFIGURE_BUILD_DIRECTORY)) {
+    New-Item -ItemType directory -Path $CONFIGURE_BUILD_DIRECTORY | Out-Null
+    if (-not $?) {
+        Write-Output "Failed to create build directory '$CONFIGURE_BUILD_DIRECTORY'"
+        exit 1
+    }
+}
+
+if ((Get-Command "cl.exe" -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Output "Setting Visual Studio environment variables"
+
+    $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+    $vcvarspath = &$vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+
+    foreach($_ in cmd /c "`"$vcvarspath\VC\Auxiliary\Build\vcvars64.bat`" > nul 2>&1 & SET") {
+        if ($_ -match '^([^=]+)=(.*)') {
+            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+        }
+    }
+}
+
+Set-Location -Path $CONFIGURE_REPOSITORY
+
+$MAKEFILE_TEXT = @"
+.SUFFIXES:
+
+build: .phony
+	@echo Building
+
+install:
+	@echo Installing to $CONFIGURE_PREFIX\lib\cmake\build
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\module
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\template
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\toolchain
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\toolchain\clang
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\toolchain\gcc
+	@mkdir $CONFIGURE_PREFIX\lib\cmake\build\toolchain\msvc
+
+clean:
+	@echo Cleaning
+
+.phony:
+"@
+
+Set-Content -Path $CONFIGURE_REPOSITORY\makefile -Value $MAKEFILE_TEXT
+
+Write-Output "OK."
+
---
+++ makefile
@@ -0,0 +1,12 @@
+.SUFFIXES:
+
+build: .phony
+	@echo Building
+
+install:
+	@echo Installing to C:\gde\lib\cmake
+
+clean:
+	@echo Cleaning
+
+.phony:
